c067bcc08bb27bee0e91b97e010521ca11c386b8
hhzxa <zxcvzxcv9699@qq.com>
2025-11-17 21:35:55 +0800

    1

--- spring-cloud-eureka/order-service/src/main/java/com/bit/order/config/BeanConfig.java
+++ spring-cloud-eureka/order-service/src/main/java/com/bit/order/config/BeanConfig.java
@@ -1,12 +1,16 @@
 package com.bit.order.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@LoadBalancerClient(name = "product-service", configuration = CustomLoadBalancerConfiguration.class)
 public class BeanConfig {
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
--- spring-cloud-eureka/order-service/src/main/java/com/bit/order/service/OrderService.java
+++ spring-cloud-eureka/order-service/src/main/java/com/bit/order/service/OrderService.java
@@ -3,6 +3,7 @@ package com.bit.order.service;
 import com.bit.order.mapper.OrderMapper;
 import com.bit.order.model.OrderInfo;
 import com.bit.order.model.ProductInfo;
+import jakarta.annotation.PostConstruct;
 import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
@@ -20,13 +21,15 @@ public class OrderService {
     private RestTemplate restTemplate;
     @Autowired
     private DiscoveryClient discoveryClient;
+    private List<ServiceInstance> instances;
+    @PostConstruct
+    public void init() {
+        instances = discoveryClient.getInstances("product-service");
+    }
     public OrderInfo selectById(Integer orderId) {
         OrderInfo orderInfo =  orderMapper.selectById(orderId);
 //        String url = "http://127.0.0.1:9090/product/" + orderInfo.getProductId();
-        List<ServiceInstance> instances = discoveryClient.getInstances("product-service");
-        String uri = instances.get(0).getUri().toString();
-
-        String url = uri + "/product/" + orderInfo.getProductId();
+        String url = "http://product-service/product/" + orderInfo.getProductId();
         ProductInfo productInfo = restTemplate.getForObject(url,ProductInfo.class);
         orderInfo.setProductInfo(productInfo);
         return orderInfo;
